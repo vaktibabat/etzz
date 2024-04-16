26a6508b899c464912bc23667ec1b647ba918e6a
vaktibabat <65859125+vaktibabat@users.noreply.github.com>
2024-04-16 18:23:48 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -29,27 +29,3 @@ fn main() {
     println!("The Resulting Decision Tree is {}", dt);
     println!("10-Fold cross validation score is {}", k_fold_cross_validation(&mut dt, 10, &dataset));
 }
-
-#[test]
-fn prediction_test() {
-    let dataset = iris::parse_dataset(
-        "/home/sag0li/Projects/Rust_DecisionTree/decisiontree/datasets/iris.csv",
-    );
-    let mut dt = decision_tree::DecisionTree::new(2);
-    dt.fit(&dataset);
-    let prediction = dt.predict(&[1.3f64, 2.4f64, 4.5f64, 2.3f64]);
-    assert_eq!(prediction, 2);
-    let prediction = dt.predict(&[1.7, 1.8, 2.3, 4.5]);
-    assert_eq!(prediction, 0);
-}
-
-#[test]
-fn k_fold_test() {
-    let dataset = iris::parse_dataset(
-        "/home/sag0li/Projects/Rust_DecisionTree/decisiontree/datasets/iris.csv",
-    );
-    let mut dt = decision_tree::DecisionTree::new(2);
-    let k_fold = k_fold_cross_validation(&mut dt, 10, &dataset);
-
-    println!("K-fold cross validation score: {}", k_fold);
-}
